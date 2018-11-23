7731ddd9fead1d8912ec1a44b04b91f28b26c519
Octavian Vaideanu <octavianvaideanu@Octavians-MacBook-Pro.local>
2018-11-23 11:20:06 +0200

    auth utils

--- routes/open-routes.js
+++ routes/open-routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../utils/is-auth');
 
 const homeController = require('../controllers/homeController');
 const spendingTypeController = require('../controllers/spendingType');
@@ -12,7 +13,7 @@ router.post('/login', homeController.login);
 router.get('/register', homeController.registerView);
 router.post('/register', homeController.registerUser);
 
-router.get('/create-spending-type', spendingTypeController.view);
-router.post('/create-spending-type', spendingTypeController.createSpendingType);
+router.get('/create-spending-type', isAuth, spendingTypeController.view);
+router.post('/create-spending-type', isAuth, spendingTypeController.createSpendingType);
 
 module.exports = router;
